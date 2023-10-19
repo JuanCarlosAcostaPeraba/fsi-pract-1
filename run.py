82b5f8a06da34a8f19879ba26d0c991acdeebdeb
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2023-10-19 10:48:42 +0100

    refactor: Terminal output

--- run.py
+++ run.py
@@ -4,66 +4,66 @@ import search, time
 
 # A -> B
 ab = search.GPSProblem('A', 'B', search.romania)
-print("\n\t\t---- [A -> B] ----")
+print("\n\t\t\t---- [A -> B] ----")
 inicio = time.time()
-print("\n\t-Amplitud-")
+print("\n\t\t-Amplitud-")
 print("Ruta: " + str(search.breadth_first_graph_search(ab).path()))
 print("Tiempo de ejecución Amplitud: " + str(time.time() - inicio))
 inicio = time.time()
 
-print("\n\t-Profundidad-")
+print("\n\t\t-Profundidad-")
 print("Ruta: " + str(search.depth_first_graph_search(ab).path()))
 print("Tiempo de ejecución Profundidad: " + str(time.time() - inicio))
 
 # O -> E
 oe = search.GPSProblem('O', 'E', search.romania)
-print("\n\t\t---- [O -> E] ----")
+print("\n\t\t\t---- [O -> E] ----")
 inicio = time.time()
-print("\n\t-Amplitud-")
+print("\n\t\t-Amplitud-")
 print("Ruta: " + str(search.breadth_first_graph_search(oe).path()))
 print("Tiempo de ejecución Amplitud: " + str(time.time() - inicio))
 inicio = time.time()
 
-print("\n\t-Profundidad-")
+print("\n\t\t-Profundidad-")
 print("Ruta: " + str(search.depth_first_graph_search(oe).path()))
 print("Tiempo de ejecución Profundidad: " + str(time.time() - inicio))
 
 # G -> Z
 gz = search.GPSProblem('G', 'Z', search.romania)
-print("\n\t\t---- [G -> Z] ----")
+print("\n\t\t\t---- [G -> Z] ----")
 inicio = time.time()
-print("\n\t-Amplitud-")
+print("\n\t\t-Amplitud-")
 print("Ruta: " + str(search.breadth_first_graph_search(gz).path()))
 print("Tiempo de ejecución Amplitud: " + str(time.time() - inicio))
 inicio = time.time()
 
-print("\n\t-Profundidad-")
+print("\n\t\t-Profundidad-")
 print("Ruta: " + str(search.depth_first_graph_search(gz).path()))
 print("Tiempo de ejecución Profundidad: " + str(time.time() - inicio))
 
 # N -> D
 nd = search.GPSProblem('N', 'D', search.romania)
-print("\n\t\t---- [N -> D] ----")
+print("\n\t\t\t---- [N -> D] ----")
 inicio = time.time()
-print("\n\t-Amplitud-")
+print("\n\t\t-Amplitud-")
 print("Ruta: " + str(search.breadth_first_graph_search(nd).path()))
 print("Tiempo de ejecución Amplitud: " + str(time.time() - inicio))
 inicio = time.time()
 
-print("\n\t-Profundidad-")
+print("\n\t\t-Profundidad-")
 print("Ruta: " + str(search.depth_first_graph_search(nd).path()))
 print("Tiempo de ejecución Profundidad: " + str(time.time() - inicio))
 
 # M -> F
 mf = search.GPSProblem('M', 'F', search.romania)
-print("\n\t\t---- [M -> F] ----")
+print("\n\t\t\t---- [M -> F] ----")
 inicio = time.time()
-print("\n\t-Amplitud-")
+print("\n\t\t-Amplitud-")
 print("Ruta: " + str(search.breadth_first_graph_search(mf).path()))
 print("Tiempo de ejecución Amplitud: " + str(time.time() - inicio))
 inicio = time.time()
 
-print("\n\t-Profundidad-")
+print("\n\t\t-Profundidad-")
 print("Ruta: " + str(search.depth_first_graph_search(mf).path()))
 print("Tiempo de ejecución Profundidad: " + str(time.time() - inicio))
 
