d713b74e16c001eba952e358ec50fc6a5a78ac0f
IsaiCordero <isaicordero03@gmail.com>
2023-11-02 10:19:06 +0000

    Run Branch & Bound

--- run.py
+++ run.py
@@ -15,6 +15,14 @@ print("\n\t\t-Profundidad-")
 print("Ruta: " + str(search.depth_first_graph_search(ab).path()))
 print("Tiempo de ejecución Profundidad: " + str(time.time() - inicio))
 
+print("\n\t\t-Branch & Bound-")
+print("Ruta: " + str(search.bab(ab).path()))
+print("Tiempo de ejecución Profundidad: " + str(time.time() - inicio))
+
+print("\n\t\t-Branch & Bound subestimación-")
+print("Ruta: " + str(search.babsub(ab).path()))
+print("Tiempo de ejecución Profundidad: " + str(time.time() - inicio))
+
 # O -> E
 oe = search.GPSProblem('O', 'E', search.romania)
 print("\n\t\t\t---- [O -> E] ----")
@@ -26,6 +34,14 @@ inicio = time.time()
 
 print("\n\t\t-Profundidad-")
 print("Ruta: " + str(search.depth_first_graph_search(oe).path()))
+print("Tiempo de ejecución Profundidad: " + str(time.time() - inicio))
+
+print("\n\t\t-Branch & Bound-")
+print("Ruta: " + str(search.bab(oe).path()))
+print("Tiempo de ejecución Profundidad: " + str(time.time() - inicio))
+
+print("\n\t\t-Branch & Bound subestimación-")
+print("Ruta: " + str(search.babsub(oe).path()))
 print("Tiempo de ejecución Profundidad: " + str(time.time() - inicio))
 
 # G -> Z
@@ -41,6 +57,14 @@ print("\n\t\t-Profundidad-")
 print("Ruta: " + str(search.depth_first_graph_search(gz).path()))
 print("Tiempo de ejecución Profundidad: " + str(time.time() - inicio))
 
+print("\n\t\t-Branch & Bound-")
+print("Ruta: " + str(search.bab(gz).path()))
+print("Tiempo de ejecución Profundidad: " + str(time.time() - inicio))
+
+print("\n\t\t-Branch & Bound subestimación-")
+print("Ruta: " + str(search.babsub(gz).path()))
+print("Tiempo de ejecución Profundidad: " + str(time.time() - inicio))
+
 # N -> D
 nd = search.GPSProblem('N', 'D', search.romania)
 print("\n\t\t\t---- [N -> D] ----")
@@ -52,6 +76,14 @@ inicio = time.time()
 
 print("\n\t\t-Profundidad-")
 print("Ruta: " + str(search.depth_first_graph_search(nd).path()))
+print("Tiempo de ejecución Profundidad: " + str(time.time() - inicio))
+
+print("\n\t\t-Branch & Bound-")
+print("Ruta: " + str(search.bab(nd).path()))
+print("Tiempo de ejecución Profundidad: " + str(time.time() - inicio))
+
+print("\n\t\t-Branch & Bound subestimación-")
+print("Ruta: " + str(search.babsub(nd).path()))
 print("Tiempo de ejecución Profundidad: " + str(time.time() - inicio))
 
 # M -> F
@@ -67,3 +99,10 @@ print("\n\t\t-Profundidad-")
 print("Ruta: " + str(search.depth_first_graph_search(mf).path()))
 print("Tiempo de ejecución Profundidad: " + str(time.time() - inicio))
 
+print("\n\t\t-Branch & Bound-")
+print("Ruta: " + str(search.bab(mf).path()))
+print("Tiempo de ejecución Profundidad: " + str(time.time() - inicio))
+
+print("\n\t\t-Branch & Bound subestimación-")
+print("Ruta: " + str(search.babsub(mf).path()))
+print("Tiempo de ejecución Profundidad: " + str(time.time() - inicio))
